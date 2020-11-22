e79a4dfb6a9c00c928af0d75f8d37a4d24768680
Alexey Samosadov <a.samosadov@basis.center>
2020-11-22 16:02:50 +0300

    module1-task3. fix bugs

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,72 +1,25 @@
 'use strict';
 const fs = require(`fs`);
 
-const DEFAULT_COUNT = 1;
-const FILE_NAME = `mocks.json`;
+const {
+  getRandomInt,
+  shuffle,
+} = require(`../../utils`);
 
 const {
-  ExitCode
+  ExitCode,
+  TITLES,
+  DEFAULT_COUNT,
+  GENERATED_FILE_NAME,
+  SENTENCES,
+  IMAGE_FILE_NAMES,
+  CATEGORIES
 } = require(`../../constants`);
 
 const PictureRestrict = {
   MIN: 1,
   MAX: 1000,
 }
-
-const {
-  getRandomInt,
-  shuffle,
-} = require(`../../utils`);
-
-const TITLES = [
-  'Продам книги Стивена Кинга.',
-  'Продам новую приставку Sony Playstation 5.',
-  'Продам отличную подборку фильмов на VHS.',
-  'Куплю антиквариат.',
-  'Куплю породистого кота.',
-  'Продам коллекцию журналов «Огонёк».',
-  'Отдам в хорошие руки подшивку «Мурзилка».',
-  'Продам советскую посуду. Почти не разбита.',
-  'Куплю детские санки.',
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-  'Две страницы заляпаны свежим кофе.',
-  'Кажется, что это хрупкая вещь.',
-  'Мой дед не мог её сломать.',
-  'Кому нужен этот новый телефон, если тут такое...',
-  'Не пытайтесь торговаться. Цену вещам я знаю.',
-];
-
-const IMAGE_FILE_NAMES = [
-  `tvovar_v_otlochnom_sostoyanii`,
-  `peregno_and_good_conditions`,
-  `sell_with_heart_hurt`,
-  'buy_antique',
-  'buy_cat_breed'
-]
-
-
-
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
 
 const OfferType = {
   OFFER: `offer`,
@@ -81,7 +34,6 @@ const SumRestrict = {
 const getPictureFileName = (i) => {
   return IMAGE_FILE_NAMES[i];
 }
-
 
 const generateOffers = (count) => (
   Array(count).fill({}).map(() => ({
@@ -99,13 +51,13 @@ module.exports = {
   run(args) {
     const [count] = args;
     if (count > 1000) {
-      console.log('Не больше 1000 объявлений');
+      console.log(`Не больше 1000 объявлений`);
       process.exit(ExitCode.error);
     }
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     const content = JSON.stringify(generateOffers(countOffer));
 
-    fs.writeFile(FILE_NAME, content, (err) => {
+    fs.writeFile(GENERATED_FILE_NAME, content, (err) => {
       if (err) {
         return console.error(`Can't write data to file...`);
       }
